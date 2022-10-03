06dbd136e742832c7c33354d8294b3b3582f99a0
WSG434 <WorkStationGuard@gmail.com>
2022-10-04 01:02:08 +0300

    Получилось на реальных данных! Отправил запрос, получил ответ, отобразил на экране

--- src/form.js
+++ src/form.js
@@ -8,7 +8,8 @@ const colors = [
   '#8e44ad',
   '#3498db',
   '#e67e22',
-  '#2ecc71']
+  '#2ecc71'];
+
 
 //Наполним форму шифрами
 const inputArr = [
@@ -18,6 +19,9 @@ const inputArr = [
   ["3", "6", "5", "5", "5", "1", "6", "5", "6", "3"],
   ["4", "2", "1", "3", "6", "3", "3", "4", "5", "6"],
 ];
+
+//Наборы букв
+const claim = ["АВГ", "ЕИК", "ЛНО", "ПРС", "ТУЩ", "ЬЯ"];
 
 //Заголовок
 const crossword_header = document.createElement('div');
@@ -57,7 +61,7 @@ crossword.append(crossword_final);
 //Кнопка для разгадывания
 const crossword_button = document.createElement('a');
 crossword_button.classList.add("crossword__button");
-crossword_button.href = "./request.php";
+// crossword_button.href = "./request.php";
 crossword_button.innerText = "Разгадать";
 crossword.append(crossword_button);
 
@@ -67,7 +71,7 @@ crossword.append(crossword_button);
 let element;
 let color;
 let finalWordElement;
-const finalWord = [
+const finalWordPos = [
   [[0, 4], [3, 3], [4, 7]],
   [[2, 3], [3, 9], [4, 9]],
   [[1, 6], [2, 1], [3, 0]]
@@ -80,7 +84,7 @@ for (let i = 0; i < 3; i++) {
     finalWordElement.style.backgroundColor = `${color}`;
     finalWordElement.style.boxShadow = `0 0 3px ${color}, 0 0 15px ${color}`;
 
-    element = document.querySelector(`#element_${finalWord[i][j][0]}${finalWord[i][j][1]}`);
+    element = document.querySelector(`#element_${finalWordPos[i][j][0]}${finalWordPos[i][j][1]}`);
     element.style.backgroundColor = `${color}`;
     element.style.boxShadow = `0 0 3px ${color}, 0 0 15px ${color}`;
   }
@@ -88,3 +92,55 @@ for (let i = 0; i < 3; i++) {
 
 
 //Отправляю запрос на сервер
+
+let obj = {
+  "inputArr": inputArr,
+  "claim": claim
+}
+
+let myJson = JSON.stringify(obj);
+
+let jsonGET;
+let resultArr;
+let finalWord;
+const spinner = document.querySelector(".spinner");
+
+crossword_button.addEventListener("click", e => {
+  spinner.classList.remove("hidden");
+  $.ajax({
+    url: "ajaxJSON.php", //Страница к которой обращаемся
+    type: "POST", //Тип запроса
+    dataType: "text",
+    data: { //Параметры запроса
+      myData: myJson
+    },
+    success: function (res) {
+      // В случае удачного обращения мы получаем данные
+      // от test.php в переменную res
+      //Выполняем какие то действия..
+      // $("#preloader").hide(); //Скрыли гифку, с id preloader
+      spinner.classList.add("hidden");
+      // $("#test").html(res); //Добавили код, который получили со страницы test.php
+      // $("#my_div").html("Parsing is DONE; CROSSWORD SOLVED"); // STOP SPINNER.
+      jsonGET = JSON.parse(res);
+      resultArr = jsonGET["resultArr"];
+      finalWord = jsonGET["finalWord"];
+      console.log(resultArr);
+      console.log(finalWord);
+      //Заполняю доску ответами из полученного массива
+      for (let i = 0; i < column; i++) {
+        for (let j = 0; j < row; j++) {
+          element = document.querySelector(`#element_${i}${j}`);
+          element.innerHTML = resultArr[i][j];
+        }
+      }
+      //Заполняю результирующеее слово
+      for (let i = 0; i < finalCol; i++) {
+        element = document.querySelector(`#final_${i}`);
+        element.innerHTML = finalWord[i];
+      }
+    }
+  });
+})
+
+
