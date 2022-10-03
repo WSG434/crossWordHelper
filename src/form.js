89e30c9d81ee02c40a333219018511633c3cc6ae
WSG434 <WorkStationGuard@gmail.com>
2022-10-03 22:51:19 +0300

    Разукрасил клетки финального слова

--- src/form.js
+++ src/form.js
@@ -3,6 +3,21 @@ const board = document.querySelector("#board");
 const crossword = document.querySelector("#crossword");
 const column = 5;
 const row = 10;
+const colors = [
+  '#e74c3c',
+  '#8e44ad',
+  '#3498db',
+  '#e67e22',
+  '#2ecc71']
+
+//Наполним форму шифрами
+const inputArr = [
+  ["1", "1", "5", "3", "2", "4", "1", "5", "2", "6"],
+  ["1", "6", "5", "6", "3", "3", "5", "3", "6", "1"],
+  ["5", "4", "2", "4", "2", "5", "1", "3", "2", "2"],
+  ["3", "6", "5", "5", "5", "1", "6", "5", "6", "3"],
+  ["4", "2", "1", "3", "6", "3", "3", "4", "5", "6"],
+];
 
 //Заголовок
 const crossword_header = document.createElement('div');
@@ -19,6 +34,7 @@ for (let i = 0; i < column; i++) {
     const square = document.createElement('div');
     square.classList.add("square");
     square.id = `element_${i}${j}`;
+    square.innerText = inputArr[i][j];
     crossword_row.append(square);
   }
   crossword.append(crossword_row);
@@ -45,3 +61,27 @@ crossword_button.href = "./request.php";
 crossword_button.innerText = "Разгадать";
 crossword.append(crossword_button);
 
+
+
+//Добавляем цвета для финальног ослова
+let element;
+let color;
+let finalWordElement;
+const finalWord = [
+  [[0, 4], [3, 3], [4, 7]],
+  [[2, 3], [3, 9], [4, 9]],
+  [[1, 6], [2, 1], [3, 0]]
+]
+
+for (let i = 0; i < 3; i++) {
+  color = colors[i];
+  for (let j = 0; j < 3; j++) {
+    finalWordElement = document.querySelector(`#final_${3 * i + j}`);
+    finalWordElement.style.backgroundColor = `${color}`;
+    finalWordElement.style.boxShadow = `0 0 3px ${color}, 0 0 15px ${color}`;
+
+    element = document.querySelector(`#element_${finalWord[i][j][0]}${finalWord[i][j][1]}`);
+    element.style.backgroundColor = `${color}`;
+    element.style.boxShadow = `0 0 3px ${color}, 0 0 15px ${color}`;
+  }
+}
